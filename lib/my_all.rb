b4d627adc39ababa0013ff623c9e8e2b2e03f5a4
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-14 01:34:09 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,29 +4,13 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-  block_return_values << yield(collection[i])
-  i = i + 1
+    block_return_values << yield(collection[i])
+    i = i + 1
   end
-end
-if block_return_values.include?(false)
+ 
+  if block_return_values.include?(false)
     false
   else
     true
   end
 end
-
-#my_all? {|i| i < collection.length}
-  
-#my_all? {|i| i < collection.length}
-#my_all?(collection) {|i| i < collection.length}
-
-#def my_all?([1,2,3])
- # i = 0
-  #while i < 3
-   # yield(1)
-    #i = 0 + 1
-  #end
-#end
-
-#my_all?([1,2,3]) {|1| 1 < 2}
-
