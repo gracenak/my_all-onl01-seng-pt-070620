5a94c385b717339e8f018dcdcf7deeebecc249cf
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-14 00:45:18 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,25 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  while i < collection.length
+  yield(collection[i])
+  i = i + 1
+  end
 end
+
+collection.any? {|i| i < collection.length}
+  
+
+#my_all?(collection) {|i| i < collection.length}
+
+#def my_all?([1,2,3])
+ # i = 0
+  #while i < 3
+   # yield(1)
+    #i = 0 + 1
+  #end
+#end
+
+#my_all?([1,2,3]) {|1| 1 < 2}
+
